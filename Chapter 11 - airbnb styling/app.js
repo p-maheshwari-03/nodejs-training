e0c1f0ec171b80d49e565c392ede307658fc79ee
pranav-maheshwari-toddleapp <pranav.maheshwari@toddleapp.com>
2025-09-23 20:42:06 +0530

    Chapter 11 - airbnb styling

---
+++ Chapter 11 - airbnb styling/app.js
@@ -0,0 +1,31 @@
+// Core modules
+const path = require("path");
+
+// External modules
+const express = require("express");
+
+// Local modules
+const rootDir = require("./utils/pathUtil");
+const userRouter = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
+
+const PORT = 3000;
+const app = express();
+
+// Parse body data
+app.use(express.urlencoded());
+
+// Serve static files
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port http://localhost:${PORT}`);
+});
---
+++ Chapter 11 - airbnb styling/utils/pathUtil.js
@@ -0,0 +1,4 @@
+// Core modules
+const path = require("path");
+
+module.exports = path.dirname(require.main.filename);
